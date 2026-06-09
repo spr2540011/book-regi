112a247923a10937c04336938061f4ee94ca6b3c
spr2540011 <spr2540011@stu.o-hara.ac.jp>
2026-06-09 18:25:23 +0900

    fix package.json

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/list",(req,res)=>{
-    const filePath="./book.json";
+    const filePath="./books.json";
 
     try{
         if(fs.existsSync(filePath)){
